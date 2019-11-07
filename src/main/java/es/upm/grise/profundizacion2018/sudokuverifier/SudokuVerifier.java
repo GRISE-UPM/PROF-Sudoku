e750a7429033a8891d7fd7c9659074ddb3ad5067
Gonzalo SPE <g.secheguren@alumnos.upm.es>
2019-11-07 23:03:00 +0100

    Ejercicio resuelto

--- src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
+++ src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
@@ -1,9 +1,58 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
+import java.util.Arrays;
+
 public class SudokuVerifier 
 {
 	public int verify (String candidateSolution)
     {
-        return 0;
-    }
+		String regex = "[1-9]+";
+        
+        if (candidateSolution.length()!=81 || !candidateSolution.matches(regex)) {
+        	return -1; // Si la longitud no es 81 es que algun valor es vacio
+        }
+        
+		int[][] sudoku = new int[9][9];
+		for (int i=0;i<9;i++) { 
+		    for (int j=0;j<9;j++) {
+		        sudoku[i][j] = Character.getNumericValue(candidateSolution.charAt(i*9+j));
+		    }
+		}
+		
+		   for (int i = 0; i < 9; i++) {
+		        int[] square = new int[9];
+		        for (int j = 0; j < 9; j ++) {
+		            square[j] = sudoku[(i / 3) * 3 + j / 3][i * 3 % 9 + j % 3];
+		        }
+		        if (!(validate(square))) return -2;
+		    }
+		   
+		   for (int i = 0; i < 9; i++) {
+		        int[] row = new int[9];
+		        for (int j = 0; j < 9; j ++) {
+		        	row[j] = sudoku[j][i];
+		        }
+		        if (!(validate(row))) return -3;
+		    }
+		  
+		   for (int i = 0; i < 9; i++) {
+		        int[] column = new int[9];
+		        for (int j = 0; j < 9; j ++) {
+		        	column[j] = sudoku[i][j];
+		        }
+		        if (!(validate(column))) return -4;
+		    }
+
+		   return 0;
+	    }
+	
+	private boolean validate(int[] check) {
+	    int i = 0;
+	    Arrays.sort(check);
+	    for (int number : check) {
+	        if (number != ++i)
+	            return false;
+	    }
+	    return true;
+	}
 }
--- src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
@@ -1,13 +1,50 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
-import static org.junit.Assert.fail;
-
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class SudokuVerifierTest {
     
+	SudokuVerifier prueba = new SudokuVerifier();
+	
 	@Test
-	public void test() {
-    		fail("Not yet implemented");
+	public void pruebaCorrecta() {
+		String sudoku = "735614892"+"842973561"+"961285374"+
+						"286349157"+"413857926"+"579126438"+
+						"157492683"+"694738215"+"328561749";
+		assertEquals(0, prueba.verify(sudoku));
     }
+	
+	@Test
+	public void pruebaIncorrectaFormato() {
+		String sudoku = "000000000"+"842973561"+"961285374"+
+						"286349157"+"413857926"+"579126438"+
+						"157492683"+"694738215"+"328561749";
+		assertEquals(-1, prueba.verify(sudoku));
+    }
+	
+	@Test
+	public void pruebaIncorrectaTamaño() {
+		String sudoku = "000000000"+"842973561"+"961285374"+
+						"286349157"+"413857926"+"579126438";
+		assertEquals(-1, prueba.verify(sudoku));
+    }
+	
+	@Test
+	public void pruebaIncorrectaCuadricula() {
+		String sudoku = "775614892"+"842973561"+"961285374"+
+						"286349157"+"413857926"+"579126438"+
+						"157492683"+"694738215"+"328561749";
+		assertEquals(-2, prueba.verify(sudoku));
+    }
+
+	@Test
+	public void pruebaIncorrectaFila() {
+		String sudoku = "683614892"+"215973561"+"749285374"+
+						"286349157"+"413857926"+"579126438"+
+						"157492683"+"694738215"+"328561749";
+		assertEquals(-3, prueba.verify(sudoku));
+    }
+	
+	// No se puede hacer una prueba de columna, ya que va a fallar la fila
 }
