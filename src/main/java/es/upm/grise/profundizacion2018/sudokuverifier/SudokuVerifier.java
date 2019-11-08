fa034672ec46a416b2a698a232e7456d48e79b60
Rafata8 <43552392+Rafata8@users.noreply.github.com>
2019-11-08 20:05:34 +0100

    ejercicio resuelto

--- src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
+++ src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
@@ -1,9 +1,77 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 public class SudokuVerifier 
 {
 	public int verify (String candidateSolution)
     {
-        return 0;
+		//(se ha preferido hacer en este orden porque es mas facil detectar las dos primeras)
+		// si se desea cambiar solo habria que cambiar el orden de los bucles
+		//de este modo el orden de deteccion es: cantidad corredcta y numeros, filas, columnas, y por ultimo subgrip
+		
+		
+		
+		///R1
+		if (!candidateSolution.matches("[1-9]+") || candidateSolution.length() != 81) {
+			return -1;
+		}
+		
+		//R3 filas
+		for(int i=0;i<9;i++) {
+			String fila=candidateSolution.substring(i*9, i*9+9);
+			System.out.println(fila);
+			if (duplicados(fila)==true)
+				return -3;
+		}
+		
+		//R4 columnas
+		for (int j=0;j<9;j++) {
+			String columna="";
+			for (int i=0;i<9;i++) {
+				columna+=candidateSolution.charAt(i*9+j);
+			}
+			if (duplicados(columna)==true)
+				return -4;
+		}		
+		
+	
+
+		
+		//R2  sub-grids     
+		for (int j=0;j<9;j++) {
+			String grid="";
+			int repe=j%3;
+			int filas =(int) j/3;
+			for (int i=0;i<3;i++) {
+				grid+=candidateSolution.substring(9*i+9*filas+repe*3,9*i+filas*9+repe*3+3);
+			}
+			if (duplicados(grid)==true)
+				return -2;
+		}
+		
+
+		
+		return 0;
     }
+	
+	//metodo de apoyo para detectar duplicados en una cadena de 9 numeros
+	public boolean duplicados(String cadena) {
+		boolean dups=false;
+	    char[] chars = cadena.toCharArray();
+	    
+	    HashSet<Character> h = new HashSet<Character>(); 
+	    //Print duplicate characters
+
+	    
+	    for (char c : chars) { 
+	    	if (h.add(c) == false) { 
+	    		return true;
+	    	}
+	    }
+
+
+	    return false;
+	}
 }
--- src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
@@ -1,13 +1,70 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
-import static org.junit.Assert.fail;
-
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class SudokuVerifierTest {
-    
+
+	SudokuVerifier prueba = new SudokuVerifier();
+
+	
 	@Test
-	public void test() {
-    		fail("Not yet implemented");
+	public void falloR1Tamaño() {
+		String sudoku = "478";
+		assertEquals(-1, prueba.verify(sudoku));
     }
+	
+	@Test
+	public void falloR1Letras() {
+		String sudoku = "A00000000"+"000000000"+"000000000"+
+				"000000000"+"0000A0000"+"000000000"+
+				"000000000"+"000000000"+"000000000";
+		assertEquals(-1, prueba.verify(sudoku));
+    }
+	
+	
+	
+	
+	
+	
+	// la fila de en medio tiene dos 1s
+	@Test
+	public void falloR3() {
+		String sudoku = "123456789"+"123456789"+"123456789"+
+				"123456789"+"123456781"+"123456789"+
+				"123456789"+"123456789"+"123456789";
+		assertEquals(-3, prueba.verify(sudoku));
+    }
+	
+	// la fila de en medio colapsa con la primera en la 5ta columna (al menos)
+	@Test
+	public void falloR4() {
+		String sudoku = "123456789"+"234567891"+"345678912"+
+				"456789123"+"912354678"+"567891234"+
+				"678912345"+"789123456"+"891234567";
+		assertEquals(-4, prueba.verify(sudoku));
+    }
+	
+	// error subgrip
+	@Test
+	public void falloR2() {
+		String sudoku = "123456789"+"234567891"+"345678912"+
+				"456789123"+"912345678"+"567891234"+
+				"678912345"+"789123456"+"891234567";
+		assertEquals(-2, prueba.verify(sudoku));
+    }
+	
+	
+	
+	
+	@Test
+	public void pruebaCorrecta() {
+		String sudoku = "123456789"+"789123456"+"456789123"+
+						"312645978"+"978312645"+"645978312"+
+						"231564897"+"897231564"+"564897231";
+		assertEquals(0, prueba.verify(sudoku));
+    }
+
+
+	 
 }
