1cd1b32dbe287962693d2b70c390370a4406d4c7
Ignacio Ripoll Ramirez <ignacioripollramirez@gmail.com>
2019-11-09 01:39:04 +0100

    Ejercicio resuelto

--- src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
+++ src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
@@ -2,8 +2,60 @@ package es.upm.grise.profundizacion2018.sudokuverifier;
 
 public class SudokuVerifier 
 {
-	public int verify (String candidateSolution)
+	public int verify (String candidateSolution) throws Exception
     {
-        return 0;
-    }
+		char [] numeros = new char [81];
+		int [][] tablero = new int [9][9];
+		
+		if(candidateSolution.length() != 81 || candidateSolution == null)
+			throw new Exception();
+		
+		
+		//regla 1:
+		if(!candidateSolution.matches("[1-9]+")){
+			return -1;
+		}
+		
+		
+		
+		//Montar el tablero
+		for( int i = 0; i<candidateSolution.length(); i++) { 
+			numeros[i]=candidateSolution.charAt(i);
+		}
+		for (int j =0; j<9;j++)
+			for (int i = 0; i<9; i++) {
+				tablero[j][i] = Character.getNumericValue(numeros[(j*9)+i]);
+		}
+		
+		
+		// regla 2:
+		for(int row = 0; row < 9; row += 3)
+		   for(int col = 0; col < 9; col += 3)
+		      for(int pos = 0; pos < 8; pos++)
+		         for(int pos2 = pos + 1; pos2 < 9; pos2++)
+		            if(tablero[row + pos%3][col + pos/3]==tablero[row + pos2%3][col + pos2/3])
+		               return -2;
+		
+		// regla 3:
+		for(int row = 0; row < 9; row++)
+		   for(int col = 0; col < 8; col++)
+		      for(int col2 = col + 1; col2 < 9; col2++)
+		         if(tablero[row][col]==tablero[row][col2]) { 
+		        	 System.out.println(row);
+		            return -3;
+		         }
+		         
+		         
+
+		// regla 4:
+		for(int col = 0; col < 9; col++)
+		   for(int row = 0; row < 8; row++)
+		      for(int row2 = row + 1; row2 < 9; row2++)
+		         if(tablero[row][col]==tablero[row2][col])
+		            return -4;
+			
+		
+		return 0;
+    } 
+	
 }
--- src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
@@ -1,13 +1,142 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
-import static org.junit.Assert.fail;
-
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SudokuVerifierTest {
     
-	@Test
-	public void test() {
-    		fail("Not yet implemented");
-    }
+SudokuVerifier objeto;
+	
+	@Before
+	public void setup() {
+		objeto = new SudokuVerifier();
+	}
+	
+	
+	@Test //Se ha insertado el numero '0' al principio de la solucion.
+	public void testHayUnCero() throws Exception {
+		String solucion = "045892367"
+		 				+ "627134958"
+		 				+ "893567124"
+		 				+ "259413876"
+		 				+ "376958241"
+		 				+ "418276593"
+		 				+ "964385712"
+		 				+ "731629485"
+		 				+ "582741639";
+		assertEquals(objeto.verify(solucion),-1,0);
+	}
+	
+	
+	@Test //Se ha insertado la letra 'a' al principio de la solucion.
+	public void testHayUnaLetra() throws Exception {
+		String solucion = "a45892367"
+ 						+ "627134958"
+ 						+ "893567124"
+ 						+ "259413876"
+ 						+ "376958241"
+ 						+ "418276593"
+ 						+ "964385712"
+ 						+ "731629485"
+ 						+ "582741639";
+		assertEquals(objeto.verify(solucion),-1,0);
+	}
+	
+	
+	@Test //Se ha insertado el signo '-' al principio de la solucion.
+	public void testHayUnSigno() throws Exception {
+		String solucion = "-45892367"
+ 						+ "627134958"
+ 						+ "893567124"
+ 						+ "259413876"
+ 						+ "376958241"
+ 						+ "418276593"
+ 						+ "964385712"
+ 						+ "731629485"
+ 						+ "582741639";
+		assertEquals(objeto.verify(solucion),-1,0);
+	}
+	
+	
+	@Test(expected = Exception.class) //solucion inicializada a null.
+	public void testNull() throws Exception {
+		String solucion = null;
+		objeto.verify(solucion);
+	}
+	
+	
+	@Test(expected = Exception.class) //Se ha quitado una fila entera.
+	public void testTamanoDistinto() throws Exception {
+		String solucion = "-45892367"
+						+ "627134958"
+						+ "893567124"
+						+ "259413876"
+						+ "376958241"
+						+ "418276593"
+
+						+ "731629485"
+						+ "582741639";
+		objeto.verify(solucion);
+	} 
+	
+	
+	@Test //Se repite el numero '2' de la primera fila.
+	public void testFilaRepetida() throws Exception {
+		String solucion = "125892367"
+		 				+ "647134958"
+		 				+ "893567124"
+		 				+ "259413876"
+		 				+ "376958241"
+		 				+ "418276593"
+		 				+ "964385712"
+		 				+ "731629485"
+		 				+ "582741639";
+		assertEquals(objeto.verify(solucion),-3,0);
+	}
+	
+	
+	@Test //Se repite el numero '1' en la primera columna.
+	public void testColumnaRepetida() throws Exception {
+		String solucion = "145892367"
+ 						+ "627134958"
+ 						+ "893567124"
+ 						+ "159413876"
+ 						+ "376958241"
+ 						+ "428276593"
+ 						+ "964385712"
+ 						+ "731629485"
+ 						+ "582741639";
+		assertEquals(objeto.verify(solucion),-3,0);
+	}
+	
+	@Test //Se repite el numero '1' en el segundo sub-grid (centro-arriba).
+	public void testGridRepetido() throws Exception {
+		String solucion = "145192367"
+		 				+ "627134958"
+		 				+ "893567124"
+		 				+ "259413876"
+		 				+ "376958241"
+		 				+ "418276593"
+		 				+ "964385712"
+		 				+ "731629485"
+		 				+ "582741639";
+		assertEquals(objeto.verify(solucion),-2,0);
+	}
+	
+	
+	@Test //solucion correcta introducida
+	public void testCorrecto() throws Exception {
+		String solucion = "145892367"
+				 		+ "627134958"
+				 		+ "893567124"
+				 		+ "259413876"
+				 		+ "376958241"
+				 		+ "418276593"
+				 		+ "964385712"
+				 		+ "731629485"
+				 		+ "582741639";
+
+		assertEquals(objeto.verify(solucion),0,0);
+	}
 }
