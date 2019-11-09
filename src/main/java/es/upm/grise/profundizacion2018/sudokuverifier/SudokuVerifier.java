efd33bc2486260f05c304870654b40be60c10304
Jaime Acosta <j.acosta@alumnos.upm.es>
2019-11-09 13:38:32 +0100

    Terminado

--- src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
+++ src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
@@ -4,6 +4,63 @@ public class SudokuVerifier
 {
 	public int verify (String candidateSolution)
     {
+		
+		// Regla 1
+		if (candidateSolution==null || candidateSolution.length()!=81 || 
+				!candidateSolution.matches("[1-9]+"))
+        return -1;
+        
+        for (int i = 0; i < 9; i++) {
+			int row_id=(int)i%9;
+			
+			//Regla 2
+			if (detectRepeat(this.getSubGrid(i, candidateSolution))) return -2;
+			//Regla 3
+			if (detectRepeat(candidateSolution.substring(row_id*9, (row_id+1)*9))) return -3;
+			//Regla 4
+			if (detectRepeat(this.getColumn(i, candidateSolution))) return -4;
+		}
+        
+        
+        
+        
         return 0;
     }
+	
+	private String getColumn(int index, String candidateSolution) {
+		if (index>9 || index <0) return null;
+		String column="";
+		for (int i = 0; i < 9; i++) {
+			column+=candidateSolution.charAt((i*9)+index);
+		}
+		
+		return column;
+	}
+	
+	private String getSubGrid(int index, String candidateSolution) {
+		
+		if (index>9 || index <0) return null;
+		String subgrid="";
+		for (int i = 0; i < 3; i++) {
+			String column=this.getColumn((index%3 * 3) + i, candidateSolution);
+			subgrid+=column.substring(((int)index/3) * 3, (((int)(index/3)) * 3)+3);
+		}
+		
+		return subgrid;
+	}
+	private boolean detectRepeat(String substring) {
+		
+		// Solo hace falta que recorra la primera mitad
+		for (int i = 0; i < 9; i++) {
+			char actual=substring.charAt(i);
+			
+			int j=i+1;
+			//Los anteriores ya se han comprobado
+			for (; j < substring.length() && actual!=substring.charAt(j) ; j++);
+			
+			if (j<substring.length()) return true;
+		}
+		
+		return false;
+	}
 }
--- src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
@@ -1,13 +1,61 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
-import static org.junit.Assert.fail;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class SudokuVerifierTest {
-    
+	SudokuVerifier prueba = new SudokuVerifier();
+	
 	@Test
-	public void test() {
-    		fail("Not yet implemented");
+	public void rule1_size() {
+		String candidateSolution = "12345678";
+		assertEquals(-1, prueba.verify(candidateSolution));
+    }
+	
+	@Test
+	public void rule1_literal() {
+		String candidateSolution = "123456789"+"123456789"+"123456789"+ "ABCDEFGHI"+
+				"123456789"+"123456789"+"123456789"+"123456789"+"123456789";
+		assertEquals(-1, prueba.verify(candidateSolution));
+    }
+	
+	@Test
+	public void rule1_zero() {
+		String candidateSolution = "123450789"+"123456789"+"123456789"+
+				"123456789"+"123456789"+"123456789"+
+				"123456789"+"123456789"+"123456789";
+		assertEquals(-1, prueba.verify(candidateSolution));
+    }
+	
+	
+	
+	@Test
+	public void rule2() {
+		String candidateSolution = "154873296"+"386592714"+"729641835"+ "863725149"+"975314628"+"412968357"+
+				"631457982"+"598236471"+/*-->*/"247189566";
+		assertEquals(-2, prueba.verify(candidateSolution));
+    }
+	
+	
+	@Test
+	public void rule3() {
+		String candidateSolution = "154873296"+"386592714"+"729641835"+ /*-->*/"873725149"+/*-->*/"965314628"+"412968357"+
+				"631457982"+"598236471"+/*-->*/"247189563";
+		assertEquals(-3, prueba.verify(candidateSolution));
+    }
+	
+	@Test
+	public void rule_4() {
+		String candidateSolution = /*-->*/ "514873296"+"386592714"+"729641835"+ "863725149"+"975314628"+"412968357"+
+				"631457982"+"598236471"+"247189563";
+		assertEquals(-4, prueba.verify(candidateSolution));
+    }
+	
+	
+	@Test
+	public void correct() {
+		String candidateSolution = "154873296"+"386592714"+"729641835"+ "863725149"+"975314628"+"412968357"+
+				"631457982"+"598236471"+"247189563";
+		assertEquals(0, prueba.verify(candidateSolution));
     }
 }
