18118e90531329c6fbeae67ced1fd766c9212db9
Antonio Gonzalez <kcalorace87@hotmail.com>
2019-11-09 21:22:27 +0100

    Finalización del ejercicio
    Realizado por: Antonio Gonzalez Sanz (090183)
     Cambios a ser confirmados:
    	modificado:     src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
    	modificado:     src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java

--- src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
+++ src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
@@ -1,9 +1,93 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
-public class SudokuVerifier 
-{
-	public int verify (String candidateSolution)
-    {
+public class SudokuVerifier {
+	final static int VALID_SUDOKU = 0;
+	final static int VIOLATION_RULE_1 = -1;
+	final static int VIOLATION_RULE_2 = -2;
+	final static int VIOLATION_RULE_3 = -3;
+	final static int VIOLATION_RULE_4 = -4;
+	
+	final static String REGEX_NUMBERS_1TO9 = "[1-9]{9}";
+	final static String REGEX_NUMBERS_NOT_REPEATING = "^(?:([1-9])(?!.*\\1))*$";
+	
+	public int verify (String candidateSolution){
+
+		if ((candidateSolution == null) || (candidateSolution.length()!=81))
+			return VIOLATION_RULE_1;
+		
+		/** Comprobamos las filas primero */
+		for (int i=0; i<81; i+=9) {
+			String subRow = candidateSolution.substring(i, i+9);
+				
+			if (!subRow.matches(REGEX_NUMBERS_1TO9)) 
+				return VIOLATION_RULE_1;
+		
+			
+			if (!subRow.matches(REGEX_NUMBERS_NOT_REPEATING)) 
+				return VIOLATION_RULE_3;
+		}
+		
+		/** Comprobamos las columnas */
+		for (int i=0; i<9; i++) {
+			String subCol = ""+ candidateSolution.charAt(i)+candidateSolution.charAt(i+9)+candidateSolution.charAt(i+18)+
+					candidateSolution.charAt(i+27)+candidateSolution.charAt(i+36)+candidateSolution.charAt(i+45)+
+					candidateSolution.charAt(i+54)+candidateSolution.charAt(i+63)+candidateSolution.charAt(i+72);
+			
+			if (!subCol.matches(REGEX_NUMBERS_NOT_REPEATING))
+				return VIOLATION_RULE_4;
+		}
+		
+		/** Comprobamos las submatrices */
+		String subMatrix1 = ""+ 
+				candidateSolution.charAt(0)+candidateSolution.charAt(1)+candidateSolution.charAt(2)+
+				candidateSolution.charAt(9)+candidateSolution.charAt(10)+candidateSolution.charAt(11)+
+				candidateSolution.charAt(18)+candidateSolution.charAt(19)+candidateSolution.charAt(20);
+		
+		String subMatrix2 = ""+ 
+				candidateSolution.charAt(3)+candidateSolution.charAt(4)+candidateSolution.charAt(5)+
+				candidateSolution.charAt(12)+candidateSolution.charAt(13)+candidateSolution.charAt(14)+
+				candidateSolution.charAt(21)+candidateSolution.charAt(22)+candidateSolution.charAt(23);
+		
+		String subMatrix3 = ""+ 
+				candidateSolution.charAt(6)+candidateSolution.charAt(7)+candidateSolution.charAt(8)+
+				candidateSolution.charAt(15)+candidateSolution.charAt(16)+candidateSolution.charAt(17)+
+				candidateSolution.charAt(24)+candidateSolution.charAt(25)+candidateSolution.charAt(26);
+		
+		String subMatrix4 = ""+ 
+				candidateSolution.charAt(27)+candidateSolution.charAt(28)+candidateSolution.charAt(29)+
+				candidateSolution.charAt(36)+candidateSolution.charAt(37)+candidateSolution.charAt(38)+
+				candidateSolution.charAt(45)+candidateSolution.charAt(46)+candidateSolution.charAt(47);
+			
+		String subMatrix5 = ""+ 
+				candidateSolution.charAt(30)+candidateSolution.charAt(31)+candidateSolution.charAt(32)+
+				candidateSolution.charAt(39)+candidateSolution.charAt(40)+candidateSolution.charAt(41)+
+				candidateSolution.charAt(48)+candidateSolution.charAt(49)+candidateSolution.charAt(50);
+		
+		String subMatrix6 = ""+ 
+				candidateSolution.charAt(33)+candidateSolution.charAt(34)+candidateSolution.charAt(35)+
+				candidateSolution.charAt(42)+candidateSolution.charAt(43)+candidateSolution.charAt(44)+
+				candidateSolution.charAt(51)+candidateSolution.charAt(52)+candidateSolution.charAt(53);
+		
+		String subMatrix7 = ""+ 
+				candidateSolution.charAt(54)+candidateSolution.charAt(55)+candidateSolution.charAt(56)+
+				candidateSolution.charAt(63)+candidateSolution.charAt(64)+candidateSolution.charAt(65)+
+				candidateSolution.charAt(72)+candidateSolution.charAt(73)+candidateSolution.charAt(74);
+		
+		String subMatrix8 = ""+ 
+				candidateSolution.charAt(57)+candidateSolution.charAt(58)+candidateSolution.charAt(59)+
+				candidateSolution.charAt(66)+candidateSolution.charAt(67)+candidateSolution.charAt(68)+
+				candidateSolution.charAt(75)+candidateSolution.charAt(76)+candidateSolution.charAt(77);
+
+		String subMatrix9 = ""+
+				candidateSolution.charAt(60)+candidateSolution.charAt(61)+candidateSolution.charAt(62)+
+				candidateSolution.charAt(69)+candidateSolution.charAt(70)+candidateSolution.charAt(71)+
+				candidateSolution.charAt(78)+candidateSolution.charAt(79)+candidateSolution.charAt(80);
+			
+		if (!subMatrix1.matches(REGEX_NUMBERS_NOT_REPEATING) || !subMatrix2.matches(REGEX_NUMBERS_NOT_REPEATING) || !subMatrix3.matches(REGEX_NUMBERS_NOT_REPEATING)||
+			!subMatrix4.matches(REGEX_NUMBERS_NOT_REPEATING) || !subMatrix5.matches(REGEX_NUMBERS_NOT_REPEATING) || !subMatrix6.matches(REGEX_NUMBERS_NOT_REPEATING)||
+			!subMatrix7.matches(REGEX_NUMBERS_NOT_REPEATING) || !subMatrix8.matches(REGEX_NUMBERS_NOT_REPEATING) || !subMatrix9.matches(REGEX_NUMBERS_NOT_REPEATING))
+			return VIOLATION_RULE_2;
+		
         return 0;
     }
 }
--- src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
@@ -1,13 +1,139 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
 public class SudokuVerifierTest {
+	
+	SudokuVerifier sudokuVerifier = new SudokuVerifier();
+	
+	/** ================ TESTS REGLA 1 ================ **/
+	@Test
+	
+	/** Evalua la regla 1 con un null */
+	public void rule1_fails_null() {
+		String sudokuNull = null;
+		assertEquals(-1,sudokuVerifier.verify(sudokuNull));						
+    }
+	
+	@Test
+	/** Evalua la regla 1 con longitud 81 pero con un 0*/
+	public void rule1_fails_zeroValue() {
+		String sudokuZeroValue = 
+				"023456789"+ //Contiene 0
+				"123456789"+
+				"123456789"+
+				"123456789"+
+				"123456789"+
+				"123456789"+
+				"123456789"+
+				"123456789"+
+				"123456789";
+		assertEquals(-1,sudokuVerifier.verify(sudokuZeroValue));						
+    }
     
 	@Test
-	public void test() {
-    		fail("Not yet implemented");
+	/** Evalua la regla 1 con un tamaño > 81*/
+	public void rule1_fails_size() {
+		String sudokuBigSize = 
+				"123456789"+
+				"123456789"+
+				"123456789"+
+				"123456789"+
+				"123456789"+
+				"123456789"+
+				"123456789"+
+				"123456789"+
+				"123456789"+
+				"123456789";
+		assertEquals(-1,sudokuVerifier.verify(sudokuBigSize));						
+    }
+	
+	@Test
+	/** Evalua la regla 1 con un tamaño = 81 pero con letras metidas en algunas posiciones */
+	public void rule1_fails_nonNumeric() {
+		String sudokuNonNumeric = 
+				"123456789"+
+				"123456789"+
+				"123456789"+
+				"12TEST789"+ //Contiene letras
+				"12TEST789"+ //Contiene letras
+				"123456789"+
+				"123456789"+
+				"123456789"+
+				"123456789";
+		assertEquals(-1,sudokuVerifier.verify(sudokuNonNumeric));						
+    }
+	
+	/** ================ TEST REGLA 2 ================ **/
+	@Test
+	/** Evalua la regla 2 con la primera submatriz con numero repetido*/
+	public void rule2_fails_repeatingNumberSubmatrix() {
+		String sudokuSubmatrixError = 
+				"123456789"+
+				"234567891"+
+				"345678912"+
+				"912345678"+
+				"567891234"+
+				"456789123"+
+				"789123456"+
+				"678912345"+
+				"891234567";
+		assertEquals(-2,sudokuVerifier.verify(sudokuSubmatrixError));						
+    }
+	
+	/** ================ TEST REGLA 3 ================ **/
+	
+	@Test
+	/** Evalua la regla 3 con numeros repetidos*/
+	public void rule3_fails_repeatingNumberRow() {
+		String sudokuRepeatingNumbersRow = 
+				"123456789"+
+				"234567891"+
+				"345678912"+ 
+				"456789123"+
+				"567891234"+
+				"678912345"+
+				"789123456"+
+				"891234567"+
+				"992345678"; //9 Se repite
+		assertEquals(-3,sudokuVerifier.verify(sudokuRepeatingNumbersRow));						
+    }
+	
+	/** ================ TEST REGLA 4 ================ **/
+	@Test
+	/** Evalua la regla 4 con columnas con numeros repetidos*/
+	public void rule4_fails_repeatingNumberCol() {
+		String sudokuRepeatingNumbersCol = 
+				"123456789"+
+				"234567891"+
+				"345678912"+ 
+				"456789123"+
+				"567891234"+
+				"678912345"+
+				"789123456"+
+				"981234567"+ //9 se repite en la primera columna para esta fila y la de abajo
+				"912345678"; 
+		assertEquals(-4,sudokuVerifier.verify(sudokuRepeatingNumbersCol));						
+    }
+	
+	/** ================ TEST SUDOKU OK ================ **/
+	
+	@Test
+	/** Evalua un sudoku que esta bien */
+	public void sudokuOK() {
+		String sudokuOK = 
+				"975218463"+
+				"486359721"+
+				"123467598"+ 
+				"791845632"+
+				"562731849"+
+				"348692175"+
+				"659124387"+
+				"237586914"+
+				"814973256"; 
+		assertEquals(0,sudokuVerifier.verify(sudokuOK));						
     }
 }
