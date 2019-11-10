b3b295f650b7cc2a9acf450e06ff9d968135697c
IsmaaelMunozAz <izzmael95@gmail.com>
2019-11-10 19:16:00 +0100

    ejercicio_acabado

--- src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
+++ src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
@@ -2,8 +2,106 @@ package es.upm.grise.profundizacion2018.sudokuverifier;
 
 public class SudokuVerifier 
 {
-	public int verify (String candidateSolution)
+	public int verify (String candidateSolution) throws Exception
     {
+		int[][]  sudoku= new int[9][9];
+		int posicion=0;
+		final int longitudIdeal=81;
+		int contador=0;
+		int longitudSudoku=candidateSolution.length();
+		if(longitudSudoku<longitudIdeal) {
+			throw new Exception();
+		}
+
+		for(int j=0; j < 9; j++){
+	        for(int k=0;k<9;k++) {
+	    		char n=candidateSolution.charAt(posicion);
+	    		if(!Character.isDigit(n) && n!='-') {
+	    			throw new Exception();
+	    		}
+	    		if(n=='-') {
+	    			int nn=candidateSolution.charAt(posicion+1);
+	    			int o=Character.getNumericValue(nn);
+		        	sudoku[j][k]=-o;
+		        	posicion=posicion+2;
+		        	contador++;
+		        	continue;
+	    		}
+	    		int o=Character.getNumericValue(n);
+	        	sudoku[j][k]=o;
+	        	contador++;
+	            posicion++;
+	         }	
+	    }
+		if(contador<longitudIdeal) {
+			throw new Exception();
+		}
+		
+        if(isNegative(sudoku)) {
+        	return -1;
+        }
+
+        if(isEqualSector(sudoku)) {
+    		return -2;
+    	}
+      
+        if(isEqualRaw(sudoku)) {
+    		return -3;
+    	}
+    	if(isEqualColum(sudoku)) {
+    		return -4;
+    	}    	
         return 0;
     }
+
+	private boolean isEqualColum(int[][] sudoku) {
+		// TODO Auto-generated method stub
+		for(int col = 0; col < 9; col++) {
+			   for(int row = 0; row < 8; row++) {
+			      for(int row2 = row + 1; row2 < 9; row2++) {
+			         if(sudoku[row][col]==sudoku[row2][col])
+			            return true;
+			      }
+			   }
+		}
+		return false;
+	}
+
+	private boolean isEqualRaw(int[][] sudoku) {
+		// TODO Auto-generated method stub
+		for(int row = 0; row < 9; row++) {
+			   for(int col = 0; col < 8; col++) {
+			      for(int col2 = col + 1; col2 < 9; col2++) {
+			         if(sudoku[row][col]==sudoku[row][col2])
+			            return true;
+			      }
+			   }
+		}
+		return false;
+	}
+
+	private boolean isEqualSector(int[][] sudoku) {
+		// TODO Auto-generated method stub
+		for(int row = 0; row < 9; row += 3) {
+			   for(int col = 0; col < 9; col += 3) {
+				   for(int pos = 0; pos < 8; pos++) {
+					   for(int pos2 = pos + 1; pos2 < 9; pos2++)
+				            if(sudoku[row + pos%3][col + pos/3]==sudoku[row + pos2%3][col + pos2/3])
+				               return true;
+				   }
+			   }
+		}
+		return false;
+	}
+
+	private boolean isNegative(int[][] sudoku) {
+		// TODO Auto-generated method stub
+		for(int row = 0; row < 9; row++) {
+			   for(int col = 0; col < 9; col++) {
+			         if(sudoku[row][col]<0)
+			            return true;
+			   }
+		}
+		return false;
+	}
 }
--- src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
@@ -1,13 +1,56 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.Assert.fail;
-
 import org.junit.Test;
 
 public class SudokuVerifierTest {
     
-	@Test
-	public void test() {
-    		fail("Not yet implemented");
-    }
+	 public SudokuVerifier sudoku=new SudokuVerifier();
+	    
+		@Test
+		public void test_longitudNoMin() {
+			String str="1234556";
+			Exception excepcion=assertThrows(Exception.class,()->sudoku.verify(str));
+			assertEquals(null, excepcion.getMessage());
+	    }
+			
+		@Test
+		public void test_longitudNoNumeric() {
+			String str="123455a6";
+			Exception excepcion=assertThrows(Exception.class,()->sudoku.verify(str));
+			assertEquals(null, excepcion.getMessage());
+	    }
+		
+		@Test
+		public void test_longitudNoPositive() throws Exception {
+			String str="11234567895112345678911234567891123456789112345678911234-567891123456789112345678-9";
+			int result=-1;
+			assertEquals(result, sudoku.verify(str));
+	    }
+		@Test
+		public void test_longitudNoGrid() throws Exception {
+			String str="123456789456123456783782123891123456789112345678911234567891123456789812345678949";
+			int result=-2;
+			assertEquals(result, sudoku.verify(str));
+	    }
+		@Test
+		public void test_longitudNoRaw() throws Exception {
+			String str="963174259178325648254689731821437596496852317735961824589713462317246985642598173";
+			int result=-3;
+			assertEquals(result, sudoku.verify(str));
+	    }
+		@Test
+		public void test_longitudNoColum() throws Exception {
+			String str="963174258178325649254689731821437596496852317735961824985713462317246985642598173";
+			int result=-4;
+			assertEquals(result, sudoku.verify(str));
+	    }
+		@Test
+		public void test_ok() throws Exception {
+			String str="963174258178325649254689731821437596496852317735961824589713462317246985642598173";
+			int result=0;
+			assertEquals(result, sudoku.verify(str));
+	    }
 }
