f46a4aa51c629e19b814b5adfe871314b2ac4662
fernando <fernando@fernando.com>
2019-11-11 19:30:22 +0100

    Add sudoku implementation and tests

--- src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
+++ src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
@@ -1,9 +1,86 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
-public class SudokuVerifier 
-{
-	public int verify (String candidateSolution)
-    {
-        return 0;
-    }
+/**
+ * 
+ * @author Fernando
+ *
+ */
+public class SudokuVerifier {
+
+	public int verify(String candidateSolution) {
+
+		if (candidateSolution == null || candidateSolution.isEmpty() || candidateSolution.length() != 81)
+			throw new IllegalArgumentException("Not valid input string length or format"); // Parse the total string
+
+		int[][] sudoku = new int[9][9];
+
+		// Parse the board R1
+		for (int i = 0; i < candidateSolution.length(); i++) {
+			int j = i / 9;
+			int k = i % 9;
+			int result;
+
+			try {
+				result = Integer.parseInt(candidateSolution.substring(i, i + 1));
+			} catch (NumberFormatException ne) {
+				return -1; // Not a valid integer from 0 to 9
+			}
+
+			sudoku[j][k] = result;
+		}
+
+		// Test the 9 subgrids R2
+		for (int grid = 0; grid < 9; grid++) {
+
+			int i = (grid / 3) * 3;
+			int j = (grid % 3) * 3;
+
+			int result = 0;
+
+			// Test 1 grid
+			for (int x = i; x < i + 3; x++) {
+				for (int y = j; y < j + 3; y++) {
+					result += sudoku[x][y];
+				}
+			}
+
+			// Sum i, i=1 to 9
+			if (result != 9 * 10 / 2) {
+				return -2; // Not a valid grid
+			}
+
+		}
+
+		// Test the rows R3
+		for (int row = 0; row < 9; row++) {
+			int result = 0;
+
+			for (int i = 0; i < 9; i++) {
+				result += sudoku[row][i];
+			}
+
+			// Sum i, i=1 to 9
+			if (result != 9 * 10 / 2) {
+				return -3; // Not a valid row
+			}
+
+		}
+
+		// Test the cols R4
+		for (int col = 0; col < 9; col++) {
+			int result = 0;
+
+			for (int i = 0; i < 9; i++) {
+				result += sudoku[i][col];
+			}
+
+			// Sum i, i=1 to 9
+			if (result != 9 * 10 / 2) {
+				return -4; // Not a valid col
+			}
+
+		}
+
+		return 0; // No errors
+	}
 }
--- src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
@@ -1,13 +1,70 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
-import static org.junit.Assert.fail;
-
+import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
+/**
+ * 
+ * @author Fernando
+ *
+ */
 public class SudokuVerifierTest {
-    
+
+	private static SudokuVerifier sv;
+
+	@BeforeClass
+	public static void setUpClass() {
+		sv = new SudokuVerifier();
+	}
+
 	@Test
-	public void test() {
-    		fail("Not yet implemented");
-    }
+	public void TestCorrectSolution() {
+		String valid_solution = "974132865635897241812645793126489357598713624743526189259378416467251938381964572";
+		assertEquals(0, sv.verify(valid_solution));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void TestStringDifferentLength() {
+		String invalid_solution = "97413286563589724181264579312648935759871362474352618";
+		sv.verify(invalid_solution);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void TestStringNull() {
+		String invalid_solution = null;
+		sv.verify(invalid_solution);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void TestStringEmpty() {
+		String invalid_solution = "";
+		sv.verify(invalid_solution);
+	}
+	
+	// Test a cell with a non integer value (voids R1)
+	@Test
+	public void TestR1NonIntegerParseableChars() {
+		String invalid_solution = "a74132865635897241812645793126489357598713624743526189259378416467251938381964572";
+		assertEquals(-1, sv.verify(invalid_solution));
+	}
+		
+	@Test
+	public void TestR2InvalidOnlySubgridRule() {
+		String invalid_solution = "123456789234567891345678912456789123567891234678912345789123456891234567912345678";
+		assertEquals(-2, sv.verify(invalid_solution));
+	}
+	
+	@Test
+	public void TestR3InvalidOnlyRowsRule() {
+		String invalid_solution = "934132865675897241812645793126489357598713624743526189259378416467251938381964572";
+		assertEquals(-3, sv.verify(invalid_solution));
+	}
+	
+	@Test
+	public void TestR4InvalidOnlyColsRule() {
+		String invalid_solution = "974132865365897241812645793126489357598713624743526189259378416467251938381964572";
+		assertEquals(-4, sv.verify(invalid_solution));
+	}
+	
 }
