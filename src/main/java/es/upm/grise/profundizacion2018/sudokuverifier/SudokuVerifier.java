969fa683462283b2cef6b412639c1c3fe81214f7
Adrian Rodriguez <ardguezsocorro@gmail.com>
2019-11-11 01:43:02 +0100

    Ejercicio 4 de pruebas para Sudoku

--- src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
+++ src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
@@ -1,9 +1,60 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
 public class SudokuVerifier 
+
+// Solución implementada sin métodos auxiliares de optimización por si acaso incumpliera esta parte del enunciado:
+ // "You should implement the functionality with a single API method with the following signature:"
+
 {
 	public int verify (String candidateSolution)
     {
+		
+		String subChar;
+		
+		// Casos no especificados se ha decidido devolver -42 
+		if(candidateSolution == null || candidateSolution.length() != 81 ) {
+			return -42;
+		}
+		
+		//valores no válidos dentro de la string
+        if(  !candidateSolution.matches("[1-9]+")) {
+        	return -1;
+        }
+        
+        
+        //subGrid
+        for(int i = 1 ; i <= 9 ; i++) {
+        	subChar = "";
+        	for(int j = 0; j <= 2 ; j++) {
+        		subChar += "" + candidateSolution.charAt(j*9) + candidateSolution.charAt(j*9+1) + candidateSolution.charAt(j*9+2);
+        	}
+        	//System.out.println(subChar);
+        	if(!subChar.contains(String.valueOf(i))) {
+    			return -2;
+    		}
+        }
+     
+        
+        //rows
+        for(int i = 0; i < 9; i++) {
+        	subChar = candidateSolution.substring(i*9,9*(i+1));
+        	for(int j = 1; j <= 9; j++) {
+        		if(!subChar.contains(String.valueOf(j))) {
+        			return -3;
+        		}
+        	}
+        }
+        
+        //columnas
+        for(int i = 1 ; i <= 9 ; i++) {
+        	for(int j = 0; j < 9 ; j++) {
+        		subChar = "" + candidateSolution.charAt(j) + candidateSolution.charAt(j+9) + candidateSolution.charAt(j+18)+ candidateSolution.charAt(j+27) + candidateSolution.charAt(j+36) + candidateSolution.charAt(j+45) + candidateSolution.charAt(j+54) + candidateSolution.charAt(j+63) + candidateSolution.charAt(j+72);
+        		if(!subChar.contains(String.valueOf(i))) {
+        			return -4;
+        		}
+        	}
+        }
+
         return 0;
     }
 }
--- src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
@@ -1,13 +1,101 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class SudokuVerifierTest {
+    public SudokuVerifier tester = new SudokuVerifier();
+    
+    
+    @Test 
+    public void casesNoSpecified() {
+    	//length != 81
+    	assertEquals("Error en la prueba para más de 81 carácteres",-42,tester.verify("123" + "456" + "789" +
+																  					  "123" + "456" + "789" +
+																					  "123" + "456" + "789" + 
+																					  "123" + "456" + "789" +
+																					  "123" + "456" + "789" +
+																					  "123" + "456" + "789" +
+																					  "123" + "456" + "789" +
+																					  "123" + "456" + "789" +
+																					  "123" + "456" + "789" +
+																					  "23"));
+    	
+    	//candidateSolution = null
+    	assertEquals("Error en la prueba para valores nulos",-42,tester.verify(null));
+    	
+    }
     
 	@Test
-	public void test() {
-    		fail("Not yet implemented");
+	public void basicStructureOfSudoku() {
+    		assertEquals("Error en prueba para valores inválidos",-1,tester.verify("012" + "345" + "678" + 
+										    									   "901" + "234" + "567" + 
+										    									   "8a0" + "123" + "4-6" + 
+										    									   "789" + "011" + "345" + 
+										    									   "678" + "901" + "234" + 
+										    									   "567" + "890" + "123" + 
+										    									   "456" + "789" + "012" + 
+										    									   "345" + "678" + "901" +
+										    									   "234" + "567" + "891" )); 		
     }
+	
+	@Test
+	public void subGridChecker() {
+		assertEquals("Prueba para subgrids",-2,tester.verify("536" + "478" + "912" +
+									  						 "672" + "195" + "348" +
+															 "198" + "342" + "567" +
+															 "859" + "761" + "423" +
+															 "426" + "853" + "791" +
+															 "713" + "924" + "856" + 
+															 "961" + "537" + "284" + 
+															 "287" + "419" + "635" + 
+															 "345" + "286" + "179"	));
+	}
+	
+	@Test
+	public void rowChecker() {
+		assertEquals("Prueba para las filas",-3,tester.verify("534" + "678" + "912" +
+									  						  "672" + "195" + "348" +
+															  "198" + "342" + "567" +
+															  "859" + "761" + "421" +
+															  "426" + "853" + "791" + 
+															  "713" + "924" + "856" + 
+															  "961" + "537" + "284" + 
+															  "287" + "419" + "635" +
+															  "345" + "286" + "179"	));
+		
+		//534678912672195348198342567859761423426853791713924856961537284287419635345286179
+	}
+	
+	
+	@Test
+	public void columnChecker() {
+		assertEquals("Prueba para las columnas",-4,tester.verify("534" + "678" + "912" +
+									  							 "672" + "195" + "348" +
+									  							 "198" + "342" + "567" + 
+																 "859" + "761" + "423" +
+																 "426" + "853" + "791" + 
+																 "713" + "924" + "856" +
+																 "961" + "537" + "824" +
+																 "287" + "419" + "635" + 
+																 "345" + "286" + "179" ));
+		
+	}
+	
+	
+	
+	@Test 
+	public void correctSudoku() {
+		assertEquals("Prueba para un sudoku bien resuelto",0,tester.verify("534" + "678" + "912" +
+																		   "672" + "195" + "348" +
+																		   "198" + "342" + "567" +
+																		   "859" + "761" + "423" +
+																		   "426" + "853" + "791" +
+																		   "713" + "924" + "856" +
+																		   "961" + "537" + "284" +
+																		   "287" + "419" + "635" +
+																		   "345" + "286" + "179"	));
+		
+	}
 }
