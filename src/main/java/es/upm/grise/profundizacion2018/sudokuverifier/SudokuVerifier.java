3dba0f306bcef4aa4e8b829060014a662c587643
RafaIG <rafael.ines.guillen@gmail.com>
2019-11-11 18:06:20 +0100

    Tarea finalizada

--- src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
+++ src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
@@ -1,9 +1,84 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
-public class SudokuVerifier 
-{
-	public int verify (String candidateSolution)
-    {
-        return 0;
-    }
+import java.util.Arrays;
+
+public class SudokuVerifier {
+
+	public int verify(String candidateSolution) {
+
+		if (candidateSolution == null || candidateSolution.length() != 81)
+			return -1;
+
+		char[] chars = candidateSolution.toCharArray();
+
+		for (char c : chars) {
+			if (!Character.isDigit(c) || c == '0')
+				return -1;
+		}
+
+		// Sub-Grid
+		int i = 0;
+		int j = 0;
+		while (i < chars.length - 20) {
+			char[] subGrid = concatenate(Arrays.copyOfRange(chars, i, i + 3), Arrays.copyOfRange(chars, i + 9, i + 12),
+					Arrays.copyOfRange(chars, i + 18, i + 21));
+			if (hasDuplicates(subGrid))
+				return -2;
+			if (j == 2) {
+				j = 0;
+				i = i + 21;
+			}
+			i = i + 3;
+			j++;
+		}
+
+		// Row
+		i = 0;
+		while (i < chars.length - 1) {
+			char[] row = Arrays.copyOfRange(chars, i, i + 9);
+			if (hasDuplicates(row))
+				return -3;
+			i = i + 9;
+		}
+
+		// Colum
+		i = 0;
+		while (i < 9) {
+			char[] colum = new char[9];
+			for (j = 0; j < colum.length - 1; j++) {
+				colum[j] = chars[i + 9 * j];
+			}
+			if (hasDuplicates(colum))
+				return -4;
+			i++;
+		}
+
+		return 0;
+	}
+
+	private boolean hasDuplicates(char[] c) {
+		for (int i = 0; i < c.length; i++) {
+			for (int j = i + 1; j < c.length; j++) {
+				if (c[i] == c[j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private char[] concatenate(char[] a, char[] b, char[] c) {
+		char[] result = new char[9];
+		int count = 0;
+		for (int i = 0; i < a.length; i++) {
+			result[count++] = a[i];
+		}
+		for (int i = 0; i < c.length; i++) {
+			result[count++] = b[i];
+		}
+		for (int i = 0; i < c.length; i++) {
+			result[count++] = c[i];
+		}
+		return result;
+	}
 }
--- src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
@@ -1,13 +1,60 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class SudokuVerifierTest {
-    
+
+	private SudokuVerifier v = new SudokuVerifier();
+
+	// Test correct
 	@Test
-	public void test() {
-    		fail("Not yet implemented");
-    }
+	public void test1() {
+		assertEquals(0, v.verify("745128369369475812812639457296741583431852796587963241924517638678394125153286974"));
+	}
+
+	// Test null
+	@Test
+	public void test2() {
+		assertEquals(-1, v.verify(null));
+	}
+
+	// Test not 81 length
+	@Test
+	public void test3() {
+		assertEquals(-1, v.verify("74541924517638678394125153286974"));
+	}
+
+	// Test with non digit characters
+	@Test
+	public void test4() {
+		assertEquals(-1, v.verify("74512836dfwcq75812812639457296741583431852796587963241924517638678394125153286974"),
+				0);
+	}
+
+	// Test with 0s
+	@Test
+	public void test5() {
+		assertEquals(-1, v.verify("745128369369475802812639457296741583431852796587963241924517638678394125153286974"),
+				0);
+	}
+
+	// Test wrong sub-grid
+	@Test
+	public void test6() {
+		assertEquals(-2, v.verify("745128369379475812812639457296741583431852796587963241924517638678394125153286974"));
+	}
+
+	// Test wrong row
+	@Test
+	public void test7() {
+		assertEquals(-3, v.verify("745728369369415812812639457296741583431852796587963241924517638678394125153286974"));
+	}
+
+	// Test wrong colum
+	@Test
+	public void test8() {
+		assertEquals(-4, v.verify("745128369369475812812639457296741583431852796785963241924517638678394125153286974"));
+	}
 }
