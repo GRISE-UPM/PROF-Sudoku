a19374e20d06a6fae69b9fd57a1b61d61bc40aac
miriamzpastor <miriam.zpastor@alumnos.upm.es>
2019-11-11 18:09:56 +0100

    Código

--- src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
+++ src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
@@ -2,8 +2,76 @@ package es.upm.grise.profundizacion2018.sudokuverifier;
 
 public class SudokuVerifier 
 {
-	public int verify (String candidateSolution)
+	public int verify (String candidateSolution)throws Exception
     {
-        return 0;
-    }
-}
+	 // Se comprueba que no sea null
+		if(candidateSolution== null) {
+			throw new Exception("String nulo");
+		}
+		
+	//Se comprueba la longitud
+		if (candidateSolution.length()!=81) {
+			throw new Exception("Longitud erronea");
+		} 
+		
+	//Solo números
+		//for(int i = 0; i<81;i++) {
+			//if (!Character.isDigit(candidateSolution.charAt(i))) {
+				//throw new Exception("Contiene datos distintos a números");
+			//}
+	//}
+		
+   	 //Pasamos a un array int el String
+   	 int [][] solucion = new int [9][9];
+   	 for (int i =0; i<9;i++) {
+   		for (int j =0; j<9;j++) {
+      		solucion[i][j]= Character.getNumericValue(candidateSolution.charAt(i*9+j));
+      	 } 
+   	 }
+
+   	 
+     // R1: En caso de tener valores que no están comprendidos entre el 1 y el 9
+   	 if ( !candidateSolution.matches("[1-9]+")) {
+       	return -1;
+       }
+	 // R2: Repetidos en el sector
+			for(int i = 0; i< 9; i += 3) {
+				   for(int j = 0; j < 9; j += 3) {
+					   for(int k = 0; k < 8; k++) {
+						   for(int l = k + 1; l < 9; l++) {
+					            if(solucion[i + k%3][j + k/3]==solucion[i + l%3][j + l/3]) {
+					               return -2;
+					       }
+					   }
+				   }
+			   }
+			}
+   	 
+	  //R3: Misma fila
+		for(int i = 0; i < 9; i++) {
+			   for(int j = 0; j < 8; j++) {
+			      for(int k = j + 1; k < 9; k++) {
+			         if(solucion[i][j]==solucion[i][k])
+			            return -3;
+			     }
+			 }
+		}
+		
+   	 //R4: Misma columna
+ 		for(int i = 0; i < 9; i++) {
+ 			   for(int j = 0; j < 8; j++) {
+ 			      for(int k = j + 1; k < 9; k++) {
+ 			         if(solucion[j][i]==solucion[k][i]) {
+ 			            return -4;
+ 			           }
+ 			         }
+ 			      }
+ 			   }
+
+   	        return 0;
+   	        
+      	 } 
+   	 }
+   	 
+
+	
--- src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
@@ -1,13 +1,61 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
 public class SudokuVerifierTest {
     
-	@Test
-	public void test() {
-    		fail("Not yet implemented");
-    }
+	public SudokuVerifier prueba =new SudokuVerifier();
+	
+	//longitud erronea
+	@Test(expected = Exception.class)
+	public void test() throws Exception{
+		String ejemplo = "1";
+		prueba.verify(ejemplo);
+	}
+	//longitud erronea
+	@Test(expected = Exception.class)
+	public void tes() throws Exception{
+		String ejemplo = null;
+		prueba.verify(ejemplo);
+	}
+	
+	//sudoku correcto
+	@Test(expected = Exception.class)
+	public void test2() throws Exception{
+		String ejemplo = "154693287632578941897124653519847326243965718768312495486759132975231864321486579";
+		assertEquals(0, prueba.verify(ejemplo),0);
+		
+	}
+	
+	//Incumple R1
+	@Test(expected = Exception.class)
+	public void test1() throws Exception{
+		String ejemplo = "1234a67891234a67891234a67891234a67891234a67891234a67891234a67891234a67891234a6789";
+		assertEquals(-1, prueba.verify(ejemplo),0);
+	}
+	//Repetidos sector
+	@Test(expected = Exception.class)
+	public void test3() throws Exception{
+		String ejemplo = "154693287132578941897124653519847326243965718768312495486759132975231864321486579";
+		assertEquals(-2, prueba.verify(ejemplo),0);
+		
+	}
+	//Repetidos fila
+	@Test(expected = Exception.class)
+	public void test5() throws Exception{
+		String ejemplo = "154653287632978941897124653519847326243965718768312495486759132975231864321486579";
+		assertEquals(-3, prueba.verify(ejemplo),0);
+		
+	}
+	//Repetidos columna
+	@Test(expected = Exception.class)
+	public void test6() throws Exception{
+		String ejemplo = "514693287632578941897124653519847326243965718768312495486759132975231864321486579";
+		assertEquals(-4, prueba.verify(ejemplo),0);
+		
+	}
+	
 }
