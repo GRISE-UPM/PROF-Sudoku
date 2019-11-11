7fe817a330ce0d10103cf574668714b1a8843939
Eduard Aymerich <eduardaymerich@gmail.com>
2019-11-11 20:49:32 +0100

    Ejercicio 4 - Eduard Aymerich

--- src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
+++ src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
@@ -1,9 +1,111 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
+import java.util.Arrays;
+
 public class SudokuVerifier 
 {
+    final int valid_solution = 0;
+
+    // Rules:
+    // R1: A cell in a Sudoku game can only store positive digits, i.e. 1…9.
+    final int violation_rule_1 = -1;
+    // R2: All digits appear only once in a sub-grid, i.e. they cannot repeat.
+    final int violation_rule_2 = -2;
+    // R3: A digit can appear only once in the rows of the global grid.
+    final int violation_rule_3 = -3;
+    // R4:  A digit can appear only once in the columns of the global grid.
+    final int violation_rule_4 = -4;
+
+    // declare sudoku grid
+    int[][] grid = new int[9][9];
+
 	public int verify (String candidateSolution)
     {
-        return 0;
+        // si es null o tamaño diferente de 9x9
+        if( (candidateSolution == null) || (candidateSolution.length() != 81) )
+        {
+            return violation_rule_1;
+        }
+        // si tiene el formato correcto
+        else
+        {
+            // pasamos el string a una matriz
+            for( int i = 0; i < 9; i++ )
+            {
+                for( int j = 0; j < 9; j++ )
+                {
+                    // get number from the string
+                    char c = candidateSolution.charAt(i*9+j);
+                    String s = String.valueOf(c);
+
+                    // check if is a valid number
+                    if( isPositiveNumber(s) ) {
+                        grid[i][j] = Character.getNumericValue(c);
+                    }
+                    else {
+                        return violation_rule_1;
+                    }
+                }
+            }
+            // validar matriz
+            return checkSudokuStatus( grid );
+        }
+    }
+
+    private static boolean isPositiveNumber(String strNum) {
+        double d;
+
+        try
+        {
+            d = Double.parseDouble(strNum);
+        }
+        catch (NumberFormatException | NullPointerException nfe)
+        {
+            return false;
+        }
+
+        return (d>0);
+    }
+
+    private int checkSudokuStatus(int[][] grid) {
+        for (int i = 0; i < 9; i++)
+        {
+            int[] row = new int[9];
+            int[] square = new int[9];
+            int[] column = grid[i].clone();
+
+            for (int j = 0; j < 9; j ++)
+            {
+                row[j] = grid[j][i];
+                square[j] = grid[(i / 3) * 3 + j / 3][i * 3 % 9 + j % 3];
+            }
+
+            if (!validate(square)) {
+                return violation_rule_2;
+            }
+            else if (!validate(row)) {
+                return violation_rule_3;
+            }
+            else if (!validate(column)) {
+                return violation_rule_4;
+            }
+        }
+        return valid_solution;
+    }
+
+    private boolean validate(int[] check)
+    {
+        int i = 0;
+
+        Arrays.sort(check);
+
+        for (int number : check)
+        {
+            if (number != ++i)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
@@ -1,13 +1,162 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
 import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class SudokuVerifierTest {
-    
+
+	SudokuVerifier sudokuVerifier = new SudokuVerifier();
+
 	@Test
-	public void test() {
-    		fail("Not yet implemented");
+	public void sudoku_null()
+	{
+		String sudoku = null;
+
+		assertEquals( -1, sudokuVerifier.verify(sudoku) );
     }
+
+	@Test
+	public void sudoku_char_in_string()
+	{
+		String sudoku = "a23456789" +
+						"123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789";
+
+		assertEquals( -1, sudokuVerifier.verify(sudoku) );
+	}
+
+	@Test
+	public void sudoku_negative_number()
+	{
+		String sudoku = "-123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789";
+
+		assertEquals( -1, sudokuVerifier.verify(sudoku) );
+	}
+
+	@Test
+	public void sudoku_no_valid_number()
+	{
+		String sudoku = "023456789" +
+						"123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789";
+
+		assertEquals( -1, sudokuVerifier.verify(sudoku) );
+	}
+
+	@Test
+	public void sudoku_size_smaller_than_81()
+	{
+		String sudoku = "12345" +
+						"12345" +
+						"12345" +
+						"12345" +
+						"12345";
+
+		assertEquals( -1, sudokuVerifier.verify(sudoku) );
+	}
+
+	@Test
+	public void sudoku_size_bigger_than_81()
+	{
+		String sudoku = "123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789" +
+						"123456789";
+
+		assertEquals( -1, sudokuVerifier.verify(sudoku) );
+	}
+
+	@Test
+	public void sudoku_repeated_number_in_subgrid()
+	{
+		String sudoku = "123456789"+
+						"234567891"+
+						"345678912"+
+						"456789123"+
+						"567891234"+
+						"678912345"+
+						"789123456"+
+						"891234567"+
+						"912345678";
+
+		assertEquals( -2, sudokuVerifier.verify(sudoku) );
+	}
+
+	@Test
+	public void sudoku_repeated_number_in_row()
+	{
+		String sudoku = "123123123"+
+						"456456456"+
+						"789789789"+
+						"123123123"+
+						"456456456"+
+						"789789789"+
+						"123123123"+
+						"456456456"+
+						"789789789";
+
+		assertEquals( -3, sudokuVerifier.verify(sudoku) );
+	}
+
+	@Test
+	public void sudoku_repeated_number_in_column()
+	{
+		String sudoku = "975218563"+
+						"486359721"+
+						"123467698"+
+						"791845632"+
+						"562731849"+
+						"348692175"+
+						"659124387"+
+						"237586914"+
+						"814973256";
+
+		assertEquals( -4, sudokuVerifier.verify(sudoku) );
+	}
+
+	@Test
+	public void sudoku_valid()
+	{
+		String sudoku = "975218463"+
+						"486359721"+
+						"123467598"+
+						"791845632"+
+						"562731849"+
+						"348692175"+
+						"659124387"+
+						"237586914"+
+						"814973256";
+
+		assertEquals( 0, sudokuVerifier.verify(sudoku) );
+	}
 }
