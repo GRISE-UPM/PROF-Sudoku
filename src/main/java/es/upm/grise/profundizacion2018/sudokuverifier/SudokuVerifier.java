189327e9c59d9616fdd7124b7f64b8bdbb8c61bf
Raul Cruz <raulcrbe@gmail.com>
2019-11-11 20:16:06 +0100

    Pruebas sudoku

--- src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
+++ src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
@@ -4,6 +4,59 @@ public class SudokuVerifier
 {
 	public int verify (String candidateSolution)
     {
+		if(candidateSolution==null || !candidateSolution.matches("[1-9]+")){
+			return -1;
+		}
+		char integers[]= candidateSolution.toCharArray();
+		if(integers.length!=81) {
+			return -1;
+		}
+		int [][] sudoku = new int[9][9];
+		int x=0;
+		for(int i = 0; i<9; i++) {
+		    for(int j = 0; j<9; j++){
+		    	sudoku[i][j] = Character.getNumericValue(integers[x]);
+		        x++;
+		    }
+		}
+		for(int i = 0; i<9; i++) {
+		    for(int j = 0; j<9; j++) {
+		    	if(isInBox(i, j,sudoku[i][j],sudoku)) return -2;
+		    	if(isInRow(i, j,sudoku[i][j],sudoku)) return -3;
+		    	if(isInCol(i, j,sudoku[i][j],sudoku)) return -4;
+		    }
+		}
+		
         return 0;
     }
+	// we check if a possible number is already in a col
+		private boolean isInRow(int row,int col, int number, int[][]board) {
+			for (int i = 0; i < 9; i++)
+				if (board[row][i] == number && col!=i)
+					return true;
+			
+			return false;
+		}
+		
+		// we check if a possible number is already in a row
+		private boolean isInCol(int row,int col,int number, int[][]board) {
+			for (int i = 0; i < 9; i++)
+				if (board[i][col] == number && row!=i)
+				return true;
+			
+			return false;
+		}
+		
+		// we check if a possible number is in its 3x3 box
+		private boolean isInBox(int row, int col, int number, int[][]board) {
+			int r = row - row % 3;
+			int c = col - col % 3;
+			for (int i = r; i < r + 3; i++)
+				for (int j = c; j < c + 3; j++) {
+					if (board[i][j] == number && (i!=row || col!=j)) 
+					return true;
+					}
+			
+			return false;
+		}
 }
--- src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
@@ -1,13 +1,58 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
 public class SudokuVerifierTest {
-    
+    //Sudoku bien hecho
 	@Test
 	public void test() {
-    		fail("Not yet implemented");
+		SudokuVerifier sudoku = new SudokuVerifier();
+		assertEquals(0,sudoku.verify("812753649943682175675491283154237896369845721287169534521974368438526917796318452"));
+		
+    }
+	//Sudoku vacio
+	@Test
+	public void test2() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		assertEquals(-1,sudoku.verify(null));
+		
+    }
+	//Sudoku con caracter alfanumérico
+	@Test
+	public void test3() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		assertEquals(-1,sudoku.verify("81a753649943682175675491283154237896369845721287169534521974368438526917796318452"));
+		
+    }
+	//Sudoku con mas de 81 caracteres
+	@Test
+	public void test4() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		assertEquals(-1,sudoku.verify("8198953649943682175675491283154237896369845721287169534521974368438526917796318452"));
+		
+    }
+	//Sudoku con error en fila
+	@Test
+	public void test5() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		assertEquals(-3,sudoku.verify("812853649943682175675491283154237896369845721287169534521974368438526917796318452"));
+		
+    }
+	//Sudoku con error en columna
+	@Test
+	public void test6() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		assertEquals(-4,sudoku.verify("182753649943682175675491283154237896369845721287169534521974368438526917796318452"));
+		
+    }
+	//Sudoku con error en grid
+	@Test
+	public void test7() {
+		SudokuVerifier sudoku = new SudokuVerifier();
+		assertEquals(-2,sudoku.verify("812753649843682175675491283154237896369845721287169534521974368438526917796318452"));
+		
     }
 }
