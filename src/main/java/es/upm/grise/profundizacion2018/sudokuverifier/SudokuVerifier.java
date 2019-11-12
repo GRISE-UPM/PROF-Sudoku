9008c4eb438ee865a7d2b38107fa00e10d378778
Esaú García Sánchez-Torija <egasato@protonmail.com>
2019-11-12 10:25:16 +0100

    Add Sudoku verifier implementation and test

--- src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
+++ src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
@@ -1,9 +1,68 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
-public class SudokuVerifier 
-{
-	public int verify (String candidateSolution)
-    {
-        return 0;
-    }
+import java.util.regex.Pattern;
+
+/**
+ * A class to verify that a Sudoku is correct.
+ */
+public class SudokuVerifier {
+
+	/**
+	 * A patern used to verify that a string contains only digits from 1 to 9.
+	 */
+	private static final Pattern ONLY_NUMBERS = Pattern.compile("^[1-9]{81}$");
+
+	/**
+	 * Given a Sudoku as a String verifies that it is correct.
+	 *
+	 * @param candidateSolution The Sudoku to check.
+	 * @return An exit status with several meanings.
+	 */
+	public int verify (final String candidateSolution) {
+
+		// En los casos no especificados he decidido devolver -69 
+		if (candidateSolution == null || candidateSolution.length() != 81 ) {
+			return -69;
+		}
+
+		// Compruebo que solo contiene números
+		if (!ONLY_NUMBERS.matcher(candidateSolution).matches()) {
+			return -1;
+		}
+
+		// Cuadrado 3x3
+		String subChar;
+		for (int i = 1; i <= 9; i++) {
+			subChar = "";
+			for (int j = 0; j <= 2; j++) {
+				subChar += "" + candidateSolution.charAt(j*9) + candidateSolution.charAt(j*9+1) + candidateSolution.charAt(j*9+2);
+			}
+			if (!subChar.contains(String.valueOf(i))) {
+				return -2;
+			}
+		}
+
+		// Filas
+		for (int i = 0; i < 9; i++) {
+			subChar = candidateSolution.substring(i * 9, (i + 1) * 9);
+			for (int j = 1; j <= 9; j++) {
+				if (!subChar.contains(String.valueOf(j))) {
+					return -3;
+				}
+			}
+		}
+
+		// Columnas
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				subChar = "" + candidateSolution.charAt(j) + candidateSolution.charAt(j+9) + candidateSolution.charAt(j+18)+ candidateSolution.charAt(j+27) + candidateSolution.charAt(j+36) + candidateSolution.charAt(j+45) + candidateSolution.charAt(j+54) + candidateSolution.charAt(j+63) + candidateSolution.charAt(j+72);
+				if (!subChar.contains(String.valueOf(i))) {
+					return -4;
+				}
+			}
+		}
+
+		return 0;
+	}
+
 }
--- src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
@@ -1,13 +1,118 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+/**
+ * Tests that the SudokuVerifier class is correct.
+ */
 public class SudokuVerifierTest {
-    
+
+	/**
+	 * The verifier instance.
+	 */
+	public SudokuVerifier tester = new SudokuVerifier();
+
 	@Test
-	public void test() {
-    		fail("Not yet implemented");
-    }
+	public void unknownChecker() {
+		final String msg = "Prueba para un Sudoku incorrecto en los casos no especificados";
+		String input = "";
+		input += "534" + "678" + "912";
+		input += "672" + "195" + "348";
+		input += "198" + "342" + "567";
+		input += "859" + "761" + "423";
+		input += "426" + "853" + "791";
+		input += "713" + "924" + "856";
+		input += "961" + "537" + "284";
+		input += "287" + "419" + "635";
+		input += "345" + "286" + "179";
+
+		input += "534" + "678" + "912";
+
+		assertEquals(msg, -69, tester.verify(null));
+		assertEquals(msg, -69, tester.verify(""));
+		assertEquals(msg, -69, tester.verify(input));
+	}
+
+	@Test
+	public void digitsChecker() {
+		final String msg = "Prueba para un Sudoku con caracteres ilegales";
+		String input = "";
+		input += "-34" + "678" + "912";
+		input += "672" + "195" + "348";
+		input += "198" + "342" + "567";
+		input += "859" + "761" + "423";
+		input += "426" + "853" + "791";
+		input += "713" + "924" + "856";
+		input += "961" + "537" + "284";
+		input += "287" + "419" + "635";
+		input += "345" + "286" + "179";
+		assertEquals(msg, -1, tester.verify(input));
+	}
+
+	@Test
+	public void squareChecker() {
+		final String msg = "Prueba para un Sudoku con uno más cuadrados de 3x3 incorrectos";
+		String input = "";
+		input += "634" + "978" + "512";
+		input += "672" + "195" + "348";
+		input += "198" + "342" + "567";
+		input += "859" + "761" + "423";
+		input += "426" + "853" + "791";
+		input += "713" + "924" + "856";
+		input += "961" + "537" + "284";
+		input += "287" + "419" + "635";
+		input += "345" + "286" + "179";
+		assertEquals(msg, -2, tester.verify(input));
+	}
+
+	@Test
+	public void rowChecker() {
+		final String msg = "Prueba para un Sudoku con una o más filas incorrectas";
+		String input = "";
+		input += "134" + "678" + "912";
+		input += "572" + "195" + "348";
+		input += "698" + "342" + "567";
+		input += "859" + "761" + "423";
+		input += "426" + "853" + "791";
+		input += "713" + "924" + "856";
+		input += "961" + "537" + "284";
+		input += "287" + "419" + "635";
+		input += "345" + "286" + "179";
+		assertEquals(msg, -3, tester.verify(input));
+	}
+
+	@Test
+	public void columnChecker() {
+		final String msg = "Prueba para un Sudoku con una o más columnas incorrectas";
+		String input = "";
+		input += "453" + "678" + "912";
+		input += "672" + "195" + "348";
+		input += "198" + "342" + "567";
+		input += "859" + "761" + "423";
+		input += "426" + "853" + "791";
+		input += "713" + "924" + "856";
+		input += "961" + "537" + "284";
+		input += "287" + "419" + "635";
+		input += "345" + "286" + "179";
+		assertEquals(msg, -4, tester.verify(input));
+	}
+
+	@Test
+	public void correctSudoku() {
+		final String msg = "Prueba para un Sudoku correcto";
+		String input = "";
+		input += "534" + "678" + "912";
+		input += "672" + "195" + "348";
+		input += "198" + "342" + "567";
+		input += "859" + "761" + "423";
+		input += "426" + "853" + "791";
+		input += "713" + "924" + "856";
+		input += "961" + "537" + "284";
+		input += "287" + "419" + "635";
+		input += "345" + "286" + "179";
+		assertEquals(msg, 0, tester.verify(input));
+	}
+
 }
