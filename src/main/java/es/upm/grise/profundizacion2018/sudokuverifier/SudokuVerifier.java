67ba9110f2e7faf8e70ad1a001715e8ce9f5f23e
JMonjo <jj.monjo@alumnos.upm.es>
2019-11-12 17:14:01 +0100

    Ejercicio Finalizado

--- src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
+++ src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
@@ -4,6 +4,66 @@ public class SudokuVerifier
 {
 	public int verify (String candidateSolution)
     {
+		/* R5: Additional.
+		 	Se ha creado esta regla para en caso de pasar una solución null
+		  		 o que la longitud no sea 81 se devuelva el resultado -5  */
+        if (candidateSolution == null || candidateSolution.length() != 81) {  	
+        	return -5;
+        }
+        
+        
+        /* R1: A cell in a Sudoku game can only store positive digits, i.e. 1…9. */
+        if (!candidateSolution.matches("[1-9]+")) {
+        	return -1;
+        }
+        
+        
+      /* R2: All digits appear only once in a sub-grid, i.e. they cannot repeat. */
+        String subgrid;
+        for (int i = 0; i < 81; i+=27) { 
+        	for (int j = 0; j < 9; j+=3) {
+        		subgrid = "";
+        		for (int k = 0; k < 27; k+=9) {
+        		subgrid += candidateSolution.charAt(i+j+k+0);
+        		subgrid += candidateSolution.charAt(i+j+k+1);
+        		subgrid += candidateSolution.charAt(i+j+k+2);
+        		}
+        		for (int k = 1; k < 10; k++) {
+        			if(!subgrid.contains(String.valueOf(k))){
+        				return -2;
+        			}
+        		}
+			}
+        }
+        
+
+        /* R3: A digit can appear only once in the rows of the global grid. */
+        String row;
+		for (int i = 0; i < 81; i+=9) {
+			row = candidateSolution.substring(i, i+9);
+			for (int j = 1; j < 10; j++) {
+				if(!row.contains(String.valueOf(j))){
+					return -3;
+				}
+			}
+		}
+		
+		
+		/* R4: A digit can appear only once in the columns of the global grid. */
+		String column;
+		for (int i = 0; i < 9; i++) {
+			column = "";
+			for (int j = 0; j < 81; j+=9)
+			column += candidateSolution.charAt(i+j);
+			for (int k = 1; k < 10; k++) {
+				if(!column.contains(String.valueOf(k))){
+					return -4;
+				}
+			}
+		}
+		
+		
+		/* All rules ok */
         return 0;
     }
 }
--- src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
@@ -1,13 +1,70 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class SudokuVerifierTest {
     
+	private SudokuVerifier sudoku;
+	
+	@Before
+	public void before() {
+		sudoku = new SudokuVerifier();
+	}
+	
+	
 	@Test
-	public void test() {
-    		fail("Not yet implemented");
+	public void test_R5a() {
+    	String solucion = null;
+    	assertEquals(-5, sudoku.verify(solucion));
+    }
+	
+	@Test
+	public void test_R5b() {
+		String solucion = "123456789" + "987654321";
+		assertEquals(-5, sudoku.verify(solucion));
+    }
+	
+	@Test
+	public void test_R1() {
+		String solucion = "28A936475" + "45927863A" + "673A54829" //Los 1 se cambian por A
+						+ "542369A87" + "3968A7542" + "8A7542396"
+						+ "A24693758" + "7684259A3" + "93578A264";
+		assertEquals(-1, sudoku.verify(solucion));
+	}
+	
+	@Test
+	public void test_R2() {
+		String solucion = "281936475" + "459278631" + "673154829"
+						+ "542369187" + "396817542" + "817542396"
+						+ "124693758" + "764825913" + "935781264"; //En el segundo 48 y deberia ser 84
+		assertEquals(-2, sudoku.verify(solucion));
+    }
+	
+	@Test
+	public void test_R3() {
+		String solucion = "281936475" + "459278631" + "673154829"
+						+ "542369182" + "396817547" + "817542396" //En el primero y segundo, ultimos digitos 2 y 7 al reves
+						+ "124693758" + "768425913" + "935781264";
+		assertEquals(-3, sudoku.verify(solucion));
+    }
+	
+	@Test
+	public void test_R4() {
+		String solucion = "281936475" + "459278631" + "673154829"
+						+ "542369187" + "396817542" + "817542396"
+						+ "124693758" + "768425913" + "935718264"; //En el ultimo se cambian 81 por 18
+		assertEquals(-4, sudoku.verify(solucion));
+    }
+	
+	@Test
+	public void test_ok() {
+		String solucion = "281936475" + "459278631" + "673154829"
+						+ "542369187" + "396817542" + "817542396"
+						+ "124693758" + "768425913" + "935781264";
+		assertEquals(0, sudoku.verify(solucion));
     }
 }
