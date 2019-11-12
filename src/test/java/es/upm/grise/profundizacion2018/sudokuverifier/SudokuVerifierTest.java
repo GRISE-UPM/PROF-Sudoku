8941bfa5a3c8469c4fb16f8568e2a06b83b6c348
jose-dp <55983469+jose-dp@users.noreply.github.com>
2019-11-12 15:09:19 +0100

    Update SudokuVerifierTest.java

--- src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
@@ -1,13 +1,45 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class SudokuVerifierTest {
     
 	@Test
-	public void test() {
-    		fail("Not yet implemented");
+	public void test1_menos81caracteres() {
+    		SudokuVerifier o = new SudokuVerifier();
+    		assertEquals(-5, o.verify("123"), 0);
+    }
+	
+	@Test
+	public void test2_correcto() {
+    		SudokuVerifier o = new SudokuVerifier();
+    		assertEquals(0, o.verify("316578492529134768487629531263415987974863125851792643138947256692351874745286319"), 0);
+    }
+	
+	
+	@Test
+	public void test3_regla1() {
+    		SudokuVerifier o = new SudokuVerifier();
+    		assertEquals(-1, o.verify("31657a492529134768487629531263415987974863125851792643138947256692351874745286319"), 0);
+    }
+	
+	@Test
+	public void test4_regla2() {
+    		SudokuVerifier o = new SudokuVerifier();
+    		assertEquals(-2, o.verify("316578492539134768487629531263415987974863125851792643138947256692351874745286319"), 0);
+    }
+	
+	@Test
+	public void test5_regla3() {
+    		SudokuVerifier o = new SudokuVerifier();
+    		assertEquals(-3, o.verify("316378492529134768487629531263415987974863125851792643138947256692351874745286319"), 0);
+    }
+	
+	@Test
+	public void test6_regla4() {
+    		SudokuVerifier o = new SudokuVerifier();
+    		assertEquals(-4, o.verify("316578492529134768487629531363415987974863125851792643138947256692351874745286319"), 0);
     }
 }
